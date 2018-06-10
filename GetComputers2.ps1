4f957fc00a05377f7dac6c0085b389538c4c666d
altralosix <33638148+altralosix@users.noreply.github.com>
2018-06-10 17:22:13 -0400

    Cleaned up

--- GetComputers2.ps1
+++ GetComputers2.ps1
@@ -1,36 +1,36 @@
+#Script to get a list of computers from an AD group, then return a list of properties from each.
+
+#Connect to DC, not bothering with credentials since I am running this on the local network and already authenticated
 $session = New-PSSession -ComputerName "losixdc1" ###-Credential $credential ####Also disabling this
 Invoke-Command $session -Scriptblock { Import-Module ActiveDirectory }
 Import-PSSession -Session $session -module ActiveDirectory
 
 
-##This works as far as just getting names of computers in the PSComputers group
+#Getting names of computers in the PSComputers group
 $workstations = (Get-ADGroupMember 'PSComputers').name
 
-
+# Get the win32_os info from each computer in the list, silently continuing if a machine is offline
 ForEach-Object {
     $wmiworkstations = Get-WmiObject -class win32_operatingsystem -cn $workstations -EA SilentlyContinue
 }
 
 
-
-$myobject = @()
+# Create an array
+$workstationOutput = @()
 ForEach ($Object in $wmiworkstations)
 {
     $Row = New-Object psobject
     $Row | Add-Member -MemberType NoteProperty -Name "Hostname" -value $Object.csname
     # Getting last time computer boot up.  By adding $var.ConvertToDateTime($var.property) we can convert the values
     $Row | Add-Member -MemberType NoteProperty -Name "LastBootTime" -value $Object.ConvertToDateTime($Object.lastbootuptime)
+    # Getting date and time Windows was installed
     $Row | Add-Member -MemberType NoteProperty -Name "Install Date" -value $Object.ConvertToDateTime($Object.installdate)
 
-    $myobject += $Row
-} ##| Format-Table -Property * -AutoSize
+    $workstationOutput += $Row
+}
 
-$myobject | Format-Table -Property * -AutoSize
-
-
-# $workstations | Format-Table -Property * -AutoSize
+$workstationOutput | Format-Table -Property * -AutoSize
 
 
-
-
+# Close the remote Powershell session
 Exit-PSSession
